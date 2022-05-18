5f50919ba14217ada2f6afd524ca7bf7b6cc76ff
Jane <janechodance@gmail.com>
2022-05-18 15:11:50 -0400

    set up redux and redux toolkit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit'
 
 import App from './components/App'
 import reducers from './reducers'
 
-const store = createStore(reducers, applyMiddleware(thunk))
+const store = configureStore({reducer: reducers})
 ReactDOM.render(
     <Provider store={store}>
         <App/>
